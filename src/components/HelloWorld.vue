4a1bedb6eed451c6e12942591c761268db419cfd
linlin <1208215066@qq.com>
2023-11-12 19:54:44 +0800

    修改了HelloWorld的组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,38 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      今天下了大雨，<br>
+      傍晚的风很凉爽。
+      <a href="https://linlin.fun" target="_blank" rel="noopener">今天还写了一些博客。</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>这是《星期六晚上我们去散步吧》里的一些很好的句子，记录一下。</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://weread.qq.com/web/bookDetail/d59326c0813ab7bbdg017221" target="_blank" rel="noopener">去试看一下</a></li>
+      <li><a href="https://weread.qq.com/" target="_blank" rel="noopener">更多书籍</a></li>
+      <li><a href="https://www.kongfz.com/" target="_blank" rel="noopener">很多纸质书籍</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>晚霞</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li>
+        天空打翻颜料
+      </li>
+      <li>
+        花了一个傍晚清洗
+      </li>
+      <li>
+        这个恶作剧
+      </li>
+      <li>
+        真令人开心
+      </li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>好看的章节</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://weread.qq.com/web/reader/d59326c0813ab7bbdg017221k8f832cb02b28f85517960bf" target="_blank" rel="noopener">一个童年</a></li>
+      <li><a href="https://weread.qq.com/web/reader/d59326c0813ab7bbdg017221ka25329702bda2557a7b2ba9" target="_blank" rel="noopener">西瓜</a></li>
+      <li><a href="https://weread.qq.com/web/reader/d59326c0813ab7bbdg017221k0e6320502c60e65972dc52f" target="_blank" rel="noopener">清醒的星星</a></li>
+      <li><a href="https://weread.qq.com/web/reader/d59326c0813ab7bbdg017221k84d326502c784d9ee44ebbe" target="_blank" rel="noopener">模仿</a></li>
+      <li><a href="https://weread.qq.com/web/reader/d59326c0813ab7bbdg017221k854328602ca854d6fae52ca" target="_blank" rel="noopener">花与花</a></li>
     </ul>
   </div>
 </template>
